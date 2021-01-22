dc374b5c350a11bf188fb35fbbec818d5dc57ce2
r3nanp <renanmol87@gmail.com>
2021-01-22 18:24:00 -0300

    feat(Header): create header

---
+++ src/Header.svelte
@@ -0,0 +1,13 @@
+<script>
+
+</script>
+
+<main>
+  <header>
+    <nav class="flex items-center border-b border-white p-2"> 
+      <h2 class="text-2xl">
+        Renan Pereira
+      </h2>
+    </nav>
+  </header>
+</main>
