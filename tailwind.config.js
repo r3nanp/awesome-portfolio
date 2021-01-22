43e1fae01947e1860a6a84de36a917cd48ebb0ed
r3nanp <renanmol87@gmail.com>
2021-01-22 18:18:56 -0300

    feat: add tailwind and postcss configuration

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+const production = !process.env.ROLLUP_WATCH
+
+module.exports = {
+  future: {
+    purgeLayersByDefault: true,
+    removeDeprecatedGapUtilities: true,
+  },
+  purge: {
+    content: ['./src/**/*.svelte', './public/**/*.html'],
+    enabled: production,
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
