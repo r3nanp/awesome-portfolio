1a48a8ab944d1107eb1edcc17e8d16c43d4a0a17
r3nanp <renanmol87@gmail.com>
2021-01-23 14:58:14 -0300

    feat(Header): add toggle component

---
+++ src/components/Header/Header.svelte
@@ -0,0 +1,13 @@
+<script context="module" lang="ts">
+  import Toggle from './Toggle.svelte'
+
+  let toggle: boolean 
+</script>
+
+<header>
+  <nav class="w-full flex items-center justify-between border-b border-white p-4">
+    <h2 class="text-2xl font-bold">Renan Pereira</h2>
+
+    <Toggle bind:toggle={toggle}  />
+  </nav>
+</header>
