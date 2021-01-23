ffa7edcced938c87d6a8593cd98e92fde8f4b603
r3nanp <renanmol87@gmail.com>
2021-01-23 14:57:44 -0300

    feat(Toggle): create a toggle button

---
+++ src/components/Header/Toggle.svelte
@@ -0,0 +1,9 @@
+<script>
+  export const toggle = false
+</script>
+
+<button
+  class="text-white hover:opacity-70 cursor-pointer w-10 h-10 focus:outline-none"
+  on:click={() => (toggle = !toggle)}>
+  <i class="fas fa-bars" />
+</button>
