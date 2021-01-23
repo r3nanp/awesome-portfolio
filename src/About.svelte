924f92e3c0a9f157bf4df8c71a426952605002ba
r3nanp <renanmol87@gmail.com>
2021-01-23 15:00:18 -0300

    feat(about): create about component

---
+++ src/About.svelte
@@ -0,0 +1,11 @@
+<main>
+  <div>
+    <a
+      href="https://linkedin.com/in/r3nanp"
+      rel="noopener noreferrer"
+      target="_blank">
+      <i class="fab fa-linkedin-in fill-current text-white" />
+      Linkedin
+    </a>
+  </div>
+</main>
