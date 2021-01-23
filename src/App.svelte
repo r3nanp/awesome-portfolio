f6967f3ad8c9a6cfbd7ecc9740a43ec6d529f49a
r3nanp <renanmol87@gmail.com>
2021-01-23 15:01:18 -0300

    feat: add API data

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,30 @@
-<script lang="ts" context="module">
+<script lang="ts">
   import Footer from './Footer.svelte'
-  import Header from './Header.svelte'
+  import Header from './components/Header/Header.svelte'
+  import { onMount } from 'svelte'
+  import type { IAPIRepos } from './types'
+
+  let repo_info: IAPIRepos[] = []
+
+  onMount(async () => {
+    const response = await fetch('https://api.github.com/users/r3nanp/repos')
+    const data = await response.json()
+
+    repo_info = data
+  })
 </script>
 
 <main class="h-screen">
   <Header />
-
+  <ul>
+    {#if repo_info}
+      {#each repo_info as { name }, i}
+        <li>{i + 1}: {name}</li>
+      {/each}
+      {:else}
+        <p class="text-2xl">Loading...</p>
+    {/if}
+  </ul>
   <Footer />
 </main>
 
