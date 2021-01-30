56e020b3f030a64dcd16b68d709225d22d2d83e6
r3nanp <renanmol87@gmail.com>
2021-01-30 12:33:41 -0300

    feat(about): add about page

--- src/About.svelte
+++ src/About.svelte
@@ -1,11 +1,18 @@
 <main>
-  <div>
+  <div class="flex align-center justify-center">
     <a
       href="https://linkedin.com/in/r3nanp"
       rel="noopener noreferrer"
       target="_blank">
       <i class="fab fa-linkedin-in fill-current text-white" />
       Linkedin
+    </a>
+    <a
+      href="https://github.com/r3nanp"
+      rel="noopener noreferrer"
+      target="_blank">
+      <i class="fab fa-github-in fill-current text-white" />
+      Github
     </a>
   </div>
 </main>
