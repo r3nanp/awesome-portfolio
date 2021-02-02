bee26cd3de2dbfc4e6d01fd5b468dcc507e7eb12
r3nanp <renanmol87@gmail.com>
2021-02-02 20:40:26 -0300

    feat(footer): remove unused tag

--- src/Footer.svelte
+++ src/Footer.svelte
@@ -1,21 +1,16 @@
-<script lang="ts">
-</script>
+<footer class="flex items-center justify-center flex-col">
+  <p class="text-md text-white">Este website foi feito com Svelte e 💖</p>
 
-<main>
-  <footer class="flex items-center justify-center flex-col">
-    <p class="text-md text-white">Este website foi feito com Svelte e 💖</p>
-
-    <a
-      href="https://github.com/r3nanp/awesome-portfolio"
-      rel="noopener noreferrer"
-      target="_blank"
-      class="bg-purple-500 px-2 py-1 my-2 rounded-xl transition hover:bg-purple-700"
-      >
-      <i class="fab fa-github fill-current text-white" />
-      Star</a
-    >
-  </footer>
-</main>
+  <a
+    href="https://github.com/r3nanp/awesome-portfolio"
+    rel="noopener noreferrer"
+    target="_blank"
+    class="bg-purple-500 px-2 py-1 my-2 rounded-xl transition hover:bg-purple-700"
+  >
+    <i class="fab fa-github fill-current text-white" />
+    Star</a
+  >
+</footer>
 
 <style>
   a:hover {
