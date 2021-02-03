9fd0ea61c9496179b5f5a4076886d4e0fbb5471d
r3nanp <renanmol87@gmail.com>
2021-02-03 20:05:05 -0300

    chore(footer): change folder to components

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,15 +1,14 @@
-<footer class="flex items-center justify-center flex-col">
+<footer class="flex items-center justify-center flex-col bg-gray-700 px-2 py-6">
   <p class="text-md text-white">Este website foi feito com Svelte e 💖</p>
 
   <a
     href="https://github.com/r3nanp/awesome-portfolio"
     rel="noopener noreferrer"
     target="_blank"
-    class="bg-purple-500 px-2 py-1 my-2 rounded-xl transition hover:bg-purple-700"
+    class="transition hover:opacity-70"
   >
-    <i class="fab fa-github fill-current text-white" />
-    Star</a
-  >
+    <i class="fab fa-github fill-current text-white text-4xl" />
+  </a>
 </footer>
 
 <style>
