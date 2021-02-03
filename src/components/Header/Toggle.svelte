2f6ad77d92b84109344c3da6a8f8eb6e4967734d
r3nanp <renanmol87@gmail.com>
2021-02-03 20:01:48 -0300

    style(toggle): change button styles

--- src/components/Header/Toggle.svelte
+++ src/components/Header/Toggle.svelte
@@ -1,9 +1,9 @@
 <script>
-  export const toggle = false
+  export let toggle = false
 </script>
 
 <button
-  class="text-white hover:opacity-70 cursor-pointer w-10 h-10 focus:outline-none"
+  class="text-white hover:opacity-70 cursor-pointer w-10 h-10 focus:outline-none md:hidden"
   on:click={() => (toggle = !toggle)}>
-  <i class="fas fa-bars" />
+  <i class="fas fa-bars fill-current text-black text-4xl" />
 </button>
