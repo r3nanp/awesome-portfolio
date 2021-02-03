23c87ac42c96b75535180e09446bc9321503ea06
r3nanp <renanmol87@gmail.com>
2021-02-03 16:04:48 -0300

    feat(about): add svelte:head

--- src/About.svelte
+++ src/About.svelte
@@ -1,3 +1,22 @@
+<svelte:head>
+  <title>Renan Pereira - About me</title>
+
+  <meta name="description" content="My portfolio using new techs!" />
+
+  <meta property="og:site_name" content="Renan Portfolio" />
+
+  <meta property="og:title" content="Renan Pereira - Portfolio" />
+  <meta property="og:description" content="My portfolio using new techs!" />
+
+  <meta property="og:image" content="../public/icon.png" />
+  <meta property="og:image:type" content="image/png" />
+
+  <meta property="og:image:width" content="1200" />
+  <meta property="og:image:height" content="630" />
+
+  <link rel="icon" href="/icon.png" />
+</svelte:head>
+
 <main>
   <div class="flex align-center justify-center">
     <a
