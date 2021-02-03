725a21541f92957ee5b4b45476f18af8e513cfe8
r3nanp <renanmol87@gmail.com>
2021-02-03 20:01:07 -0300

    style(header): create sticky header

--- src/components/Header/Header.svelte
+++ src/components/Header/Header.svelte
@@ -1,10 +1,10 @@
 <script context="module" lang="ts">
   import Toggle from './Toggle.svelte'
 
-  let toggle: boolean 
+  let toggle: boolean
 </script>
 
-<header>
+<header class="sticky top-0 z-10 bg-white">
   <nav class="w-full flex items-center justify-between border-b border-white p-4">
     <h2 class="text-2xl font-bold">Renan Pereira</h2>
 
