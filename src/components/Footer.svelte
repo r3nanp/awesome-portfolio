c2c57ab34bbb7e90fbb97d650faff36a718cce60
r3nanp <renanmol87@gmail.com>
2021-02-04 17:54:51 -0300

    feat(footer): add footer icons

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,14 +1,24 @@
 <footer class="flex items-center justify-center flex-col bg-gray-700 px-2 py-6">
   <p class="text-md text-white">Este website foi feito com Svelte e 💖</p>
 
-  <a
-    href="https://github.com/r3nanp/awesome-portfolio"
-    rel="noopener noreferrer"
-    target="_blank"
-    class="transition hover:opacity-70"
-  >
-    <i class="fab fa-github fill-current text-white text-4xl" />
-  </a>
+  <div class="flex align-center justify-center">
+    <a
+      href="https://github.com/r3nanp/awesome-portfolio"
+      rel="noopener noreferrer"
+      target="_blank"
+      class="transition hover:opacity-70 pr-2"
+    >
+      <i class="fab fa-github fill-current text-white text-4xl" />
+    </a>
+    <a
+      href="https://linkedin.com/in/r3nanp"
+      rel="noopener noreferrer"
+      target="_blank"
+      class="transition hover:opacity-70"
+    >
+      <i class="fab fa-linkedin fill-current text-white text-4xl" />
+    </a>
+  </div>
 </footer>
 
 <style>
