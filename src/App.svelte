1e9b623c8cfe299572478768d0677661718a4635
r3nanp <renanmol87@gmail.com>
2021-02-05 20:07:03 -0300

    feat(app): add sliced dynamic repos

--- src/App.svelte
+++ src/App.svelte
@@ -7,13 +7,16 @@
   import Header from './components/Header/Header.svelte';
   import Presentation from './components/Presentation.svelte';
 
-  let repo_info: IAPIRepos[] = [];
+  let repos: IAPIRepos[] = [];
 
   onMount(async () => {
     const response = await fetch('https://api.github.com/users/r3nanp/repos');
     const data = await response.json();
 
-    repo_info = data;
+    const shuffledRepos = data.sort(() => 0.5 - Math.random());
+    const slicedRepos = shuffledRepos.slice(0, 6);
+
+    repos = slicedRepos;
   });
 </script>
 
@@ -39,15 +42,27 @@
 <Header />
 <main>
   <Presentation />
-  <ul class="p-2 bg-gray-100">
-    {#if repo_info}
-      {#each repo_info as { name }, i}
-        <li transition:fly={{ x: -40, duration: 2000 }}>{i + 1}: {name}</li>
-      {/each}
-    {:else}
-      <p class="text-2xl p-2">Loading...</p>
-    {/if}
-  </ul>
+  <article class="text-center">
+    <h1 class="center text-4xl font-bold py-2">Projetos</h1>
+    <ul
+      class="p-2 bg-gray-100 grid grid-cols-1 grid-rows-4 gap-y-4 sm:px-6 md:px-8 md:grid-cols-3 md:grid-rows-1 gap-x-2"
+    >
+      {#if repos}
+        {#each repos as { name, html_url }}
+          <li
+            transition:fly={{ x: -40, duration: 2000 }}
+            class="flex justify-center md:justify-start items-center shadow-xl w-30 h-30"
+          >
+            <a href={html_url} target="_blank" rel="noopener noreferrer">
+              <strong class="text-black text-center text-xl">{name}</strong>
+            </a>
+          </li>
+        {/each}
+      {:else}
+        <p class="text-2xl p-2">Loading...</p>
+      {/if}
+    </ul>
+  </article>
 </main>
 <Footer />
 
