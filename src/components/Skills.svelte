661a70759f5bd4166cbe7036c0d052de344095c9
r3nanp <renanmol87@gmail.com>
2021-02-09 20:38:22 -0300

    feat(skills): add icons and box components

--- src/components/Skills.svelte
+++ src/components/Skills.svelte
@@ -1,46 +1,70 @@
+<script lang="ts">
+  import IconWrapper from './IconWrapper.svelte'
+  import Box from './Box.svelte'
+
+  import DiHtml5 from 'svelte-icons/di/DiHtml5.svelte'
+  import DiJsBadge from 'svelte-icons/di/DiJsBadge.svelte'
+  import DiCss3 from 'svelte-icons/di/DiCss3.svelte'
+  import DiReact from 'svelte-icons/di/DiReact.svelte'
+  import DiNodejsSmall from 'svelte-icons/di/DiNodejsSmall.svelte'
+  import DiDatabase from 'svelte-icons/di/DiDatabase.svelte'
+</script>
+
 <section class="mt-6 text-center p-10 overflow-hidden">
   <h2 class="font-bold text-4xl text-black">Habilidades</h2>
 
   <div class="flex h-auto flex-wrap mt-4">
-    <article
-      class="h-auto text-center bg-white rounded-xl p-4 flex-grow shadow m-4"
-    >
-      <i class="fab fa-js fill-current text-yellow-300 text-4xl" />
-
-      <h2 class="mt-4 text-2xl font-bold">Javascript</h2>
-      <p class="text-md font-normal">2 anos de XP</p>
-    </article>
-    <article
-      class="h-auto text-center bg-white rounded-xl p-4 flex-grow shadow m-4"
-    >
-      <i class="fab fa-html5 fill-current text-red-600 text-4xl" />
+    <Box>
+      <IconWrapper color="text-red-600">
+        <DiHtml5 />
+      </IconWrapper>
 
       <h2 class="mt-4 text-2xl font-bold">HTML</h2>
       <p class="text-md font-normal">3 anos de XP</p>
-    </article>
-    <article
-      class="h-auto text-center bg-white rounded-xl p-4 flex-grow shadow m-4"
-    >
-      <i class="fab fa-css3-alt fill-current text-blue-500 text-4xl" />
+    </Box>
+
+    <Box>
+      <IconWrapper color="text-blue-500">
+        <DiCss3 />
+      </IconWrapper>
 
       <h2 class="mt-4 text-2xl font-bold">CSS</h2>
       <p class="text-md font-normal">3 anos de XP</p>
-    </article>
-    <article
-      class="h-auto text-center bg-white rounded-xl p-4 flex-grow shadow m-4"
-    >
-      <i class="fab fa-react fill-current text-blue-500 text-4xl" />
+    </Box>
+
+    <Box>
+      <IconWrapper color="text-yellow-400">
+        <DiJsBadge />
+      </IconWrapper>
+
+      <h2 class="mt-4 text-2xl font-bold">Javascript</h2>
+      <p class="text-md font-normal">2 anos de XP</p>
+    </Box>
+
+    <Box>
+      <IconWrapper color="text-blue-500">
+        <DiReact />
+      </IconWrapper>
 
       <h2 class="mt-4 text-2xl font-bold">React</h2>
       <p class="text-md font-normal">7 meses de XP</p>
-    </article>
-    <article
-      class="h-auto text-center bg-white rounded-xl p-4 flex-grow shadow m-4"
-    >
-      <i class="fab fa-node fill-current text-green-500 text-4xl" />
+    </Box>
+
+    <Box>
+      <IconWrapper color="text-green-500">
+        <DiNodejsSmall />
+      </IconWrapper>
 
       <h2 class="mt-4 text-2xl font-bold">Node.js</h2>
       <p class="text-md font-normal">7 meses de XP</p>
-    </article>
+    </Box>
+    <Box>
+      <IconWrapper color="text-yellow-200">
+        <DiDatabase />
+      </IconWrapper>
+
+      <h2 class="mt-4 text-2xl font-bold">SQL</h2>
+      <p class="text-md font-normal">2 anos de XP</p>
+    </Box>
   </div>
 </section>
