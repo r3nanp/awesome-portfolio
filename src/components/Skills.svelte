827acf56fa3554a9422cdab73eea6efbc37794d2
r3nanp <renanmol87@gmail.com>
2021-02-08 21:50:05 -0300

    feat(skills): create skills component

---
+++ src/components/Skills.svelte
@@ -0,0 +1,46 @@
+<section class="mt-6 text-center p-10 overflow-hidden">
+  <h2 class="font-bold text-4xl text-black">Habilidades</h2>
+
+  <div class="flex h-auto flex-wrap mt-4">
+    <article
+      class="h-auto text-center bg-white rounded-xl p-4 flex-grow shadow m-4"
+    >
+      <i class="fab fa-js fill-current text-yellow-300 text-4xl" />
+
+      <h2 class="mt-4 text-2xl font-bold">Javascript</h2>
+      <p class="text-md font-normal">2 anos de XP</p>
+    </article>
+    <article
+      class="h-auto text-center bg-white rounded-xl p-4 flex-grow shadow m-4"
+    >
+      <i class="fab fa-html5 fill-current text-red-600 text-4xl" />
+
+      <h2 class="mt-4 text-2xl font-bold">HTML</h2>
+      <p class="text-md font-normal">3 anos de XP</p>
+    </article>
+    <article
+      class="h-auto text-center bg-white rounded-xl p-4 flex-grow shadow m-4"
+    >
+      <i class="fab fa-css3-alt fill-current text-blue-500 text-4xl" />
+
+      <h2 class="mt-4 text-2xl font-bold">CSS</h2>
+      <p class="text-md font-normal">3 anos de XP</p>
+    </article>
+    <article
+      class="h-auto text-center bg-white rounded-xl p-4 flex-grow shadow m-4"
+    >
+      <i class="fab fa-react fill-current text-blue-500 text-4xl" />
+
+      <h2 class="mt-4 text-2xl font-bold">React</h2>
+      <p class="text-md font-normal">7 meses de XP</p>
+    </article>
+    <article
+      class="h-auto text-center bg-white rounded-xl p-4 flex-grow shadow m-4"
+    >
+      <i class="fab fa-node fill-current text-green-500 text-4xl" />
+
+      <h2 class="mt-4 text-2xl font-bold">Node.js</h2>
+      <p class="text-md font-normal">7 meses de XP</p>
+    </article>
+  </div>
+</section>
