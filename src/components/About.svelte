6d7f7354ee7f179d49be56c321d8c6841a474e2e
r3nanp <renanmol87@gmail.com>
2021-02-28 17:12:42 -0300

    feat(about): add image and styles

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -1,18 +1,24 @@
-<section>
-  <article class="hidden md:block md:w-1/2">
-    <!-- <img src="" alt="Renan" /> -->
+<script lang="ts">
+  const avatar_url = 'https://avatars.githubusercontent.com/u/47953339?v=4'
+</script>
+
+<section
+  class="flex items-center justify-center md:grid md:grid-cols-2 md:content-center md:gap-20 md:px-10"
+>
+  <article class="hidden md:block">
+    <img src={avatar_url} alt="Renan" class="rounded-3xl" />
   </article>
 
-  <article class="w-full flex flex-col items-center text-center md:w-1/2">
+  <article class="w-full flex flex-col items-center text-center p-20">
     <h2 class="tracking-wide text-4xl font-bold md:text-center">Sobre</h2>
 
-    <p class="text-justify tracking-wide pb-2">
+    <p class="text-justify tracking-wide py-2">
       Me chamo Renan Pereira, tenho 17 anos, sou autodidata, desenvolvedor de
       software e técnico em Redes de Computadores. Apaixonado por tecnologia e
       atualmente estou estagiando na área.
     </p>
 
-    <p class="text-justify tracking-wide pb-4">
+    <p class="text-justify tracking-wide py-2">
       Desde 2018 venho estudando programação, comecei estudando PHP com HTML,
       CSS e JS, fazendo algumas landing pages usando essas tecnologias. Depois,
       aprendi Python, e atualmente estou usando Javascript e seu ecossistema.
