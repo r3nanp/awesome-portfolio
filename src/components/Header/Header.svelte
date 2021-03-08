05061afc2600acc13edecfbf012a3ad2745c7422
r3nanp <renanmol87@gmail.com>
2021-03-08 00:36:03 -0300

    feat(header): add hamburger menu and animation

--- src/components/Header/Header.svelte
+++ src/components/Header/Header.svelte
@@ -1,13 +1,24 @@
-<script context="module" lang="ts">
-  import Toggle from './Toggle.svelte'
+<script lang="ts">
+  import MdMenu from 'svelte-icons/md/MdMenu.svelte'
 
-  let toggle: boolean
+  import Sidebar from './Sidebar.svelte'
+
+  let sidebar_show = false
 </script>
 
 <header class="sticky top-0 z-10 bg-white">
-  <nav class="w-full flex items-center justify-between border-b border-white p-4">
+  <nav
+    class="w-full flex items-center justify-between border-b border-white p-4"
+  >
     <h2 class="text-2xl font-bold">Renan Pereira</h2>
 
-    <Toggle bind:toggle  />
+    <button
+      class="hover:opacity-70 cursor-pointer w-10 h-10 focus:outline-none"
+      on:click={() => (sidebar_show = !sidebar_show)}
+    >
+      <MdMenu />
+    </button>
+
+    <Sidebar bind:show={sidebar_show} />
   </nav>
 </header>
