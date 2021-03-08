eeb703c682d3baea03dc18e381b494b82f12188b
r3nanp <renanmol87@gmail.com>
2021-03-08 00:36:30 -0300

    feat(sidebar): create sidebar component

---
+++ src/components/Header/Sidebar.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import { fly } from 'svelte/transition'
+  import { useClickOutside } from '../../utils/useClickOutside'
+
+  export let show = false
+  export let show_sidebar = false
+
+  function handleClickOutside() {
+    show_sidebar = false
+    alert('Cliquei')
+  }
+</script>
+
+{#if show}
+  <nav
+    class="fixed top-0 right-0 h-full py-8 px-4 bg-white overflow-auto w-40 border-l border-gray-400"
+    transition:fly={{ x: 250, opacity: 1 }}
+  >
+    <button
+      on:click={() => {
+        show_sidebar = true
+        show = false
+      }}
+    />
+  </nav>
+{/if}
