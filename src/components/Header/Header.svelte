cea8da3bb936cbafd4769f423e8452bf6e37e9c6
r3nanp <renanmol87@gmail.com>
2021-04-07 20:57:52 -0300

    feat: add sidebar and close sidebar function

--- src/components/Header/Header.svelte
+++ src/components/Header/Header.svelte
@@ -1,9 +1,20 @@
 <script lang="ts">
   import MdMenu from 'svelte-icons/md/MdMenu.svelte'
+  const close_icon = 'images/close.svg'
 
   import Sidebar from './Sidebar.svelte'
 
   let sidebar_show = false
+
+  function click_outside(): void {
+    const element = document.querySelector('nav')
+
+    element.addEventListener('click', (event: MouseEvent) => {
+      if (event.target !== element) {
+        sidebar_show = false
+      }
+    })
+  }
 </script>
 
 <header class="sticky top-0 z-10 bg-white">
@@ -12,12 +23,14 @@
   >
     <h2 class="text-2xl font-bold">Renan Pereira</h2>
 
-    <button
-      class="hover:opacity-70 cursor-pointer w-10 h-10 focus:outline-none"
-      on:click={() => (sidebar_show = !sidebar_show)}
-    >
-      <MdMenu />
-    </button>
+    {#if !sidebar_show}
+      <button
+        class="hover:opacity-70 cursor-pointer w-10 h-10 focus:outline-none"
+        on:click={() => (sidebar_show = !sidebar_show)}
+      >
+        <MdMenu />
+      </button>
+    {/if}
 
     <Sidebar bind:show={sidebar_show} />
   </nav>
--- src/components/Header/Sidebar.svelte
+++ src/components/Header/Sidebar.svelte
@@ -1,26 +1,42 @@
 <script lang="ts">
   import { fly } from 'svelte/transition'
-  import { useClickOutside } from '../../utils/useClickOutside'
 
   export let show = false
   export let show_sidebar = false
 
-  function handleClickOutside() {
-    show_sidebar = false
-    alert('Cliquei')
+  const close_icon = 'images/close.svg'
+
+  function searchByTopic(event: Event) {
+    event.preventDefault()
+
+    const linkElement = document.querySelector('nav a [href^="#"')
+
+    linkElement.addEventListener('click', () => {
+      let id = linkElement.getAttribute('href')
+
+      // let targetOffset = id.
+    })
   }
 </script>
 
 {#if show}
   <nav
     class="fixed top-0 right-0 h-full py-8 px-4 bg-white overflow-auto w-40 border-l border-gray-400"
-    transition:fly={{ x: 250, opacity: 1 }}
+    in:fly={{ x: 150, opacity: 0.7 }}
   >
     <button
+      class="absolute bg-transparent border-0 right-2 top-0"
       on:click={() => {
         show_sidebar = true
         show = false
       }}
-    />
+    >
+      <img src={close_icon} alt="Close icon" class="h-12 w-12" /></button
+    >
+
+    <div class="flex flex-col justify-center items-center">
+      <a href="#about" class="py-2 hover:underline font-xl">Sobre</a>
+      <a href="#skills" class="py-2 hover:underline font-xl">Habilidades</a>
+    </div>
   </nav>
 {/if}
