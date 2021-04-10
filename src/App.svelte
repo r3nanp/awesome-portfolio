86ffb5778d27ad8998ad1128cd7820d16ce39aa1
r3nanp <renanmol87@gmail.com>
2021-04-10 15:03:33 -0300

    refactor: remove unused icons and add semantic html

--- src/App.svelte
+++ src/App.svelte
@@ -4,13 +4,10 @@
   import type { IAPIRepos } from './types'
 
   import Footer from './components/Footer.svelte'
-  import Header from './components/Header/Header.svelte'
+  import Header from './components/Header.svelte'
   import Presentation from './components/Presentation.svelte'
   import Skills from './components/Skills.svelte'
-  import Wave from './components/Wave.svelte'
   import About from './components/About.svelte'
-
-  import FaGithub from 'svelte-icons/fa/FaGithub.svelte'
 
   let repos: IAPIRepos[] = []
 
@@ -30,9 +27,9 @@
   <Presentation />
 
   <About />
-  <Wave>
-    <article class="bg-gray-100 text-center p-10">
-      <h1 class="center text-4xl font-bold py-2">Projetos</h1>
+  <section>
+    <article class="bg-gray-100 text-center p-10 dark:bg-gray-800">
+      <h1 class="center text-4xl font-bold py-2 dark:text-white">Projetos</h1>
       <div
         class="grid grid-cols-1 grid-rows-4 gap-y-4 sm:px-6 md:px-8 md:grid-cols-3 md:grid-rows-1 gap-x-4"
       >
@@ -40,25 +37,25 @@
           {#each repos as { name, html_url, description }}
             <div
               transition:fly={{ x: -40, duration: 2000 }}
-              class="flex justify-center bg-white flex-col items-center md:justify-start shadow-md w-32 h-44 text-center py-4 rounded m-2"
+              class="flex justify-center bg-white dark:shadow-xl dark:bg-gray-700 flex-col items-center md:justify-start shadow-md h-44 text-center py-4 rounded m-2"
             >
-              <strong class="text-black text-center text-xl">{name}</strong>
-              <p class="text-center text-md px-2">
+              <strong class="text-black dark:text-white text-center text-xl"
+                >{name}</strong
+              >
+              <p class="text-center text-md px-2 dark:text-white">
                 {description}
               </p>
-              <a href={html_url} target="_blank" rel="noopener noreferrer">
-                <button
-                  name="Github"
-                  class="flex items-center py-3 px-8 my-2 bg-blue-600 hover:bg-blue-700 w-28 h-12 rounded"
-                >
-                  <div class="w-11 h-6 fill-current text-white text-center">
-                    <FaGithub />
-                  </div>
-                  <span class="uppercase text-white font-bold text-md pr-2"
-                    >github</span
-                  >
-                </button></a
+              <a
+                href={html_url}
+                target="_blank"
+                rel="noopener noreferrer"
+                class="flex justify-center items-center py-3 px-4 my-2 bg-blue-600 hover:bg-blue-700 dark:bg-purple-600 dark:hover:bg-purple-700 w-28 h-12 rounded"
               >
+                <span
+                  class="uppercase text-center text-white font-bold text-md pr-2"
+                  >github</span
+                >
+              </a>
             </div>
           {/each}
         {:else}
@@ -69,12 +66,12 @@
         href="https://github.com/r3nanp?tab=repositories"
         target="_blank"
         rel="noopener noreferrer"
-        class="text-blue-700 tracking-wide mt-2 transition hover:underline text-xl"
+        class="text-blue-700 tracking-wide mt-2 transition hover:underline text-xl dark:text-purple-700"
       >
         Meus repositórios no Github
       </a>
     </article>
-  </Wave>
+  </section>
 
   <Skills />
 </main>
