b32b2bc52350c3ee153d6d2163a1e15ccfe36f31
r3nanp <renanmol87@gmail.com>
2021-04-10 12:24:38 -0300

    refactor: remove unused sidebar

---
+++ src/components/Header.svelte
@@ -0,0 +1,6 @@
+<header class="sticky top-0 z-10 bg-white">
+  <nav
+    class="w-full flex items-center justify-between border-b border-white p-4"
+  >
+    <h2 class="text-2xl font-bold">Renan Pereira</h2>
+</header>
