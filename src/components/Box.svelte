bde332668679546e54ae5e6b4b23e2b8a277d3c8
r3nanp <renanmol87@gmail.com>
2021-04-10 15:01:48 -0300

    style: add dark mode

--- src/components/Box.svelte
+++ src/components/Box.svelte
@@ -1,5 +1,5 @@
 <article
-  class="h-auto text-center bg-white rounded-xl p-4 flex-grow shadow m-4 transition duration-500 ease-in-out transform cursor-pointer hover:-translate-y-1 hover:scale-110"
+  class="h-auto text-center bg-white dark:bg-gray-600 rounded-xl p-4 flex-grow shadow m-4 transition duration-500 ease-in-out transform cursor-pointer hover:-translate-y-1 hover:scale-110"
 >
   <slot />
 </article>
